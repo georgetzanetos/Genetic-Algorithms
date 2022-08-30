84ada813ecc332ff13ffa0c686bfa4d0c525d194
georgetzanetos <tzortzistzanetos@gmail.com>
2022-08-30 18:57:47 +0200

    update

--- Custom_EA_mod.py
+++ Custom_EA_mod.py
@@ -5,7 +5,6 @@ import numpy as np
 
 seed=10
 np.random.seed(seed)
-random.seed(seed)
 
 #sigmoid activation function
 def sigmoid(x):
@@ -38,7 +37,7 @@ class NN_agent :
             output = np.reshape(np.matmul(output, self.weights[i]) + self.biases[i], (self.nodes[i+1]))
         return np.argmax(sigmoid(output))
 
-class Population :
+class Population:
 
     def __init__(self, n_individuals, p_mut, n_nodes):
         self.nodeCount = n_nodes
@@ -46,12 +45,24 @@ class Population :
         self.m_rate = p_mut
         self.population = [NN_agent(n_nodes) for i in range(n_individuals)]
 
+    def crossover(self,parent1,parent2):
+
+        crossover_point = np.random.randint(0, parent1.shape[0])
+        
+        child1 = parent1
+        child2 = parent2
+
+        child1[crossover_point:], child2[crossover_point:] = \
+            child2[crossover_point:], child1[crossover_point:]
+        return child1, child2
+
+
     #variation
-    def createOffspring(self, nn1, nn2):
+    def mutate(self):
 
         global seed
         
-        offspring = NN_agent(self.nodeCount)
+        offspring = self.population
 
         for i in range(len(offspring.weights)):
             for j in range(len(offspring.weights[i])):
@@ -62,9 +73,9 @@ class Population :
                         random.seed(seed)
                         offspring.weights[i][j][k] = random.uniform(-1, 1)
                         seed+=1
-                    else:
-                        offspring.weights[i][j][k] = (nn1.weights[i][j][k] + nn2.weights[i][j][k])/2.0
-                        seed+=1
+                    # else:
+                    #     offspring.weights[i][j][k] = (nn1.weights[i][j][k] + nn2.weights[i][j][k])/2.0
+                    #     seed+=1
 
         for i in range(len(offspring.biases)):
             for j in range(len(offspring.biases[i])):
@@ -74,39 +85,35 @@ class Population :
                     random.seed(seed)
                     offspring.biases[i][j] = random.uniform(-1, 1)
                     seed+=1
-                else:
-                    offspring.biases[i][j] = (nn1.biases[i][j] + nn2.biases[i][j])/2.0
-                    seed+=1
+                # else:
+                #     offspring.biases[i][j] = (nn1.biases[i][j] + nn2.biases[i][j])/2.0
+                #     seed+=1
 
         return offspring
 
     #selection
-    def newGen(self):       
-
-        global seed 
-
-        total_fitness = [0]
-        next_gen = []
-        for i in range(len(self.population)):
-            total_fitness.append(total_fitness[i]+self.population[i].fitness)
+    def newGen(self):
         
+        new_gen = []
+        new_gen.append(self.population[top_inds[-1], :])
+
+        while(len(new_gen) < self.popCount):
+            parent1 = self.population[rest_inds[random.randint()]]
+            parent2 = self.population[rest_inds[random.randint()]]
+
+            new_gen.append(crossover(parent1,parent2))
+
+        new_gen = mutate()    
         
-            random.seed(seed)
-            r1 = random.uniform(0, total_fitness[len(total_fitness)-1] )
-            seed+=1
-            random.seed(seed)
-            r2 = random.uniform(0, total_fitness[len(total_fitness)-1] )
-            seed+=1
-            nn1 = self.population[bisect.bisect_right(total_fitness, r1)-1]
-            nn2 = self.population[bisect.bisect_right(total_fitness, r2)-1]
-            next_gen.append(self.createOffspring(nn1, nn2))
         self.population.clear()
-        self.population = next_gen
+        self.population = new_gen
 
-GENERATIONS = 2
+
+GENERATIONS = 5
 STEPS = 500 
 POPULATION = 10
 MUTATION = 0.01
+n_best = 4
 
 env = gym.make('CartPole-v1')
 observation = env.reset()
@@ -118,10 +125,14 @@ pop = Population(POPULATION, MUTATION, [dim_in, 8, 8, dim_out])
 MAXFIT = []
 AVGFIT = []
 
+
 for gen in range(GENERATIONS):
     max = 0
     avg = 0
-    maxNeuralNet = None
+
+    gen_fitness = []
+
+    # maxNeuralNet = None
 
     for nn in pop.population:
         totalReward = 0
@@ -139,22 +150,36 @@ for gen in range(GENERATIONS):
         if nn.fitness > max :
             max = nn.fitness
             maxNeuralNet = nn
+        gen_fitness.append(nn.fitness)
+        
+
+    inds = np.argsort(gen_fitness)
+    print(avg)
 
     avg/=pop.popCount
     print("Generation : %3d |  Avg Fitness : %4.0f  |  Max Fitness : %4.0f  " % (gen+1, avg, max) )
     MAXFIT.append(max) 
     AVGFIT.append(avg)
+
+    
+  
+    top_inds = inds[-n_best:]
+    rest_inds = inds[0:-n_best]
+
+    print(inds)
+    print(top_inds)
+    print(rest_inds)
+
     pop.newGen()
-        
+
+
 env.close()
 
-plt.figure();
-plt.plot(range(GENERATIONS), AVGFIT)
-plt.plot(range(GENERATIONS), MAXFIT)
-plt.xlabel('Generations')
-plt.ylabel('Fitness')
-plt.legend(['Mean fitness', 'Max fitness'])
-plt.grid()
-plt.show()
-
-print(maxNeuralNet)
+# plt.figure();
+# plt.plot(range(GENERATIONS), AVGFIT)
+# plt.plot(range(GENERATIONS), MAXFIT)
+# plt.xlabel('Generations')
+# plt.ylabel('Fitness')
+# plt.legend(['Mean fitness', 'Max fitness'])
+# plt.grid()
+# plt.show()
