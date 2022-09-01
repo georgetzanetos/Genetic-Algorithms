a3f5eb0c7b51acfb80d4095ad97bf253e29faa02
georgetzanetos <tzortzistzanetos@gmail.com>
2022-09-01 12:50:28 +0200

    update

--- Custom_EA_older.py
+++ Custom_EA_older.py
@@ -4,8 +4,8 @@ import random, bisect
 import numpy as np
 
 seed=10
-# np.random.seed(seed)
-# random.seed(seed)
+np.random.seed(seed)
+random.seed(seed)
 
 #sigmoid activation function
 def sigmoid(x):
@@ -18,12 +18,10 @@ class NN_agent :
 
         global seed
 
-        self.weights = []
-        self.biases = []
-
         self.fitness = 0
         self.nodes = n_nodes
-
+        self.weights = []
+        self.biases = []
         for i in range(len(n_nodes) - 1):
             np.random.seed(seed)
             self.weights.append(np.random.uniform(low=-1, high=1, size=(n_nodes[i], n_nodes[i+1])).tolist())
@@ -43,18 +41,17 @@ class NN_agent :
 class Population :
 
     def __init__(self, n_individuals, p_mut, n_nodes):
-        
-        self.population = [NN_agent(n_nodes) for i in range(n_individuals)]
-        self.n_nodes = n_nodes
-        self.pop_size = n_individuals
+        self.nodeCount = n_nodes
+        self.popCount = n_individuals
         self.m_rate = p_mut
-        
+        self.population = [NN_agent(n_nodes) for i in range(n_individuals)]
+
     #variation
-    def createOffspring(self, parent1, parent2):
+    def createOffspring(self, nn1, nn2):
 
         global seed
         
-        offspring = NN_agent(self.n_nodes)
+        offspring = NN_agent(self.nodeCount)
 
         for i in range(len(offspring.weights)):
             for j in range(len(offspring.weights[i])):
@@ -66,7 +63,7 @@ class Population :
                         offspring.weights[i][j][k] = random.uniform(-1, 1)
                         seed+=1
                     else:
-                        offspring.weights[i][j][k] = (parent1.weights[i][j][k] + parent2.weights[i][j][k])/2.0
+                        offspring.weights[i][j][k] = (nn1.weights[i][j][k] + nn2.weights[i][j][k])/2.0
                         seed+=1
 
         for i in range(len(offspring.biases)):
@@ -78,61 +75,42 @@ class Population :
                     offspring.biases[i][j] = random.uniform(-1, 1)
                     seed+=1
                 else:
-                    offspring.biases[i][j] = (parent1.biases[i][j] + parent2.biases[i][j])/2.0
+                    offspring.biases[i][j] = (nn1.biases[i][j] + nn2.biases[i][j])/2.0
                     seed+=1
 
         return offspring
 
     #selection
-    def newGen(self, migration_ratio, elite):       
+    def newGen(self):       
 
         global seed 
 
         total_fitness = [0]
-        new_gen = []
-        
-
+        next_gen = []
         for i in range(len(self.population)):
             total_fitness.append(total_fitness[i]+self.population[i].fitness)
-            
-        while(len(new_gen) < self.pop_size*migration_ratio):
+        
+        while(len(next_gen) < self.popCount):
             random.seed(seed)
             r1 = random.uniform(0, total_fitness[len(total_fitness)-1] )
             seed+=1
             random.seed(seed)
             r2 = random.uniform(0, total_fitness[len(total_fitness)-1] )
             seed+=1
-            parent1 = self.population[bisect.bisect_right(total_fitness, r1)-1]
-            parent2 = self.population[bisect.bisect_right(total_fitness, r2)-1]
-            new_gen.append(self.createOffspring(parent1,parent2))
-
-        while(len(new_gen) < self.pop_size):
-
-            random.seed(seed)
-            r1 = random.randint(0, len(elite))
-            seed+=1
-            parent1 = self.population[r1]
-
-            random.seed(seed)
-            r2 = random.randint(0, len(elite))
-            seed+=1
-            parent2 = self.population[r2]
-
-            new_gen.append(self.createOffspring(parent1,parent2))
-
+            nn1 = self.population[bisect.bisect_right(total_fitness, r1)-1]
+            nn2 = self.population[bisect.bisect_right(total_fitness, r2)-1]
+            next_gen.append(self.createOffspring(nn1, nn2))
         self.population.clear()
-        self.population = new_gen
+        self.population = next_gen
 
+GENERATIONS = 30
 STEPS = 500 
-GENS = 30
 POPULATION = 30
 MUTATION = 0.01
-MIGRATION = 0.5
-ELITISM = 4
 
 env = gym.make('CartPole-v1')
-
 observation = env.reset()
+
 dim_in = env.observation_space.shape[0]
 dim_out = env.action_space.n
 pop = Population(POPULATION, MUTATION, [dim_in, 8, 8, dim_out])
@@ -140,48 +118,41 @@ pop = Population(POPULATION, MUTATION, [dim_in, 8, 8, dim_out])
 MAXFIT = []
 AVGFIT = []
 
-for gen in range(GENS):
+for gen in range(GENERATIONS):
     max = 0
     avg = 0
-    TopGenome = None
+    maxNeuralNet = None
 
-    for genome in pop.population:
-        Reward = 0
+    for nn in pop.population:
+        totalReward = 0
         
         for step in range(STEPS):
             env.render()
-            action = genome.getOutput(observation)
+            action = nn.getOutput(observation)
             observation, reward, done, info = env.step(action)
-            Reward += reward
+            totalReward += reward
             if done:
                 observation = env.reset()
                 break
+        nn.fitness = totalReward
+        avg += nn.fitness
+        if nn.fitness > max :
+            max = nn.fitness
+            maxNeuralNet = nn
 
-        genome.fitness = Reward
-        avg += genome.fitness
-
-        if genome.fitness > max :
-            max = genome.fitness
-            TopGenome = genome
-    
-    idx = np.argsort(avg)
-    elitidx = idx[-ELITISM:]
-
-    avg/=pop.pop_size
+    avg/=pop.popCount
     print("Generation : %3d |  Avg Fitness : %4.0f  |  Max Fitness : %4.0f  " % (gen+1, avg, max) )
     MAXFIT.append(max) 
     AVGFIT.append(avg)
-    pop.newGen(MIGRATION,elitidx)
+    pop.newGen()
         
 env.close()
 
 plt.figure();
-plt.plot(range(GENS), AVGFIT)
-plt.plot(range(GENS), MAXFIT)
+plt.plot(range(GENERATIONS), AVGFIT)
+plt.plot(range(GENERATIONS), MAXFIT)
 plt.xlabel('Generations')
 plt.ylabel('Fitness')
 plt.legend(['Mean fitness', 'Max fitness'])
 plt.grid()
 plt.show()
-
-print(TopGenome)
