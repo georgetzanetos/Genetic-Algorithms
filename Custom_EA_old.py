a46949005024cd9f6c2832957a469101a5ecf161
georgetzanetos <tzortzistzanetos@gmail.com>
2022-09-02 15:13:39 +0200

    update

--- Custom_EA_old.py
+++ Custom_EA_old.py
@@ -1,11 +1,13 @@
 import gym
 from matplotlib import pyplot as plt
-import random, bisect
+import random, bisect, timeit
 import numpy as np
 
+start = timeit.default_timer()
+
 seed=10
-np.random.seed(seed)
-random.seed(seed)
+# np.random.seed(seed)
+# random.seed(seed)
 
 #sigmoid activation function
 def sigmoid(x):
@@ -18,10 +20,12 @@ class NN_agent :
 
         global seed
 
-        self.fitness = 0
-        self.nodes = n_nodes
         self.weights = []
         self.biases = []
+
+        self.fitness = 0
+        self.nodes = n_nodes
+
         for i in range(len(n_nodes) - 1):
             np.random.seed(seed)
             self.weights.append(np.random.uniform(low=-1, high=1, size=(n_nodes[i], n_nodes[i+1])).tolist())
@@ -41,17 +45,18 @@ class NN_agent :
 class Population :
 
     def __init__(self, n_individuals, p_mut, n_nodes):
-        self.nodeCount = n_nodes
-        self.popCount = n_individuals
-        self.m_rate = p_mut
+        
         self.population = [NN_agent(n_nodes) for i in range(n_individuals)]
-
+        self.n_nodes = n_nodes
+        self.pop_size = n_individuals
+        self.m_rate = p_mut
+        
     #variation
-    def createOffspring(self, nn1, nn2):
+    def createOffspring(self, parent1, parent2):
 
         global seed
         
-        offspring = NN_agent(self.nodeCount)
+        offspring = NN_agent(self.n_nodes)
 
         for i in range(len(offspring.weights)):
             for j in range(len(offspring.weights[i])):
@@ -63,7 +68,7 @@ class Population :
                         offspring.weights[i][j][k] = random.uniform(-1, 1)
                         seed+=1
                     else:
-                        offspring.weights[i][j][k] = (nn1.weights[i][j][k] + nn2.weights[i][j][k])/2.0
+                        offspring.weights[i][j][k] = (parent1.weights[i][j][k] + parent2.weights[i][j][k])/2.0
                         seed+=1
 
         for i in range(len(offspring.biases)):
@@ -75,7 +80,7 @@ class Population :
                     offspring.biases[i][j] = random.uniform(-1, 1)
                     seed+=1
                 else:
-                    offspring.biases[i][j] = (nn1.biases[i][j] + nn2.biases[i][j])/2.0
+                    offspring.biases[i][j] = (parent1.biases[i][j] + parent2.biases[i][j])/2.0
                     seed+=1
 
         return offspring
@@ -86,11 +91,11 @@ class Population :
         global seed 
 
         total_fitness = [0]
-        next_gen = []
+        new_gen = []
         for i in range(len(self.population)):
             total_fitness.append(total_fitness[i]+self.population[i].fitness)
-        
-        
+            
+        while(len(new_gen) < self.pop_size):
             random.seed(seed)
             r1 = random.uniform(0, total_fitness[len(total_fitness)-1] )
             seed+=1
@@ -99,18 +104,18 @@ class Population :
             seed+=1
             nn1 = self.population[bisect.bisect_right(total_fitness, r1)-1]
             nn2 = self.population[bisect.bisect_right(total_fitness, r2)-1]
-            next_gen.append(self.createOffspring(nn1, nn2))
+            new_gen.append(self.createOffspring(nn1, nn2))
         self.population.clear()
-        self.population = next_gen
+        self.population = new_gen
 
-GENERATIONS = 2
 STEPS = 500 
-POPULATION = 10
+GENS = 30
+POPULATION = 40
 MUTATION = 0.01
 
 env = gym.make('CartPole-v1')
-observation = env.reset()
 
+observation = env.reset()
 dim_in = env.observation_space.shape[0]
 dim_out = env.action_space.n
 pop = Population(POPULATION, MUTATION, [dim_in, 8, 8, dim_out])
@@ -118,29 +123,31 @@ pop = Population(POPULATION, MUTATION, [dim_in, 8, 8, dim_out])
 MAXFIT = []
 AVGFIT = []
 
-for gen in range(GENERATIONS):
+for gen in range(GENS):
     max = 0
     avg = 0
-    maxNeuralNet = None
+    TopGenome = None
 
-    for nn in pop.population:
-        totalReward = 0
+    for genome in pop.population:
+        Reward = 0
         
         for step in range(STEPS):
             env.render()
-            action = nn.getOutput(observation)
+            action = genome.getOutput(observation)
             observation, reward, done, info = env.step(action)
-            totalReward += reward
+            Reward += reward
             if done:
                 observation = env.reset()
                 break
-        nn.fitness = totalReward
-        avg += nn.fitness
-        if nn.fitness > max :
-            max = nn.fitness
-            maxNeuralNet = nn
 
-    avg/=pop.popCount
+        genome.fitness = Reward
+        avg += genome.fitness
+
+        if genome.fitness > max :
+            max = genome.fitness
+            TopGenome = genome
+
+    avg/=pop.pop_size
     print("Generation : %3d |  Avg Fitness : %4.0f  |  Max Fitness : %4.0f  " % (gen+1, avg, max) )
     MAXFIT.append(max) 
     AVGFIT.append(avg)
@@ -149,12 +156,12 @@ for gen in range(GENERATIONS):
 env.close()
 
 plt.figure();
-plt.plot(range(GENERATIONS), AVGFIT)
-plt.plot(range(GENERATIONS), MAXFIT)
+plt.plot(range(GENS), AVGFIT)
+plt.plot(range(GENS), MAXFIT)
 plt.xlabel('Generations')
 plt.ylabel('Fitness')
 plt.legend(['Mean fitness', 'Max fitness'])
 plt.grid()
 plt.show()
 
-print(maxNeuralNet)
+print('Time: ', stop - start) 
